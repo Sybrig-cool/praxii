4feab3f57d63216cd6922b130a2f82ee8f3b4875
Manii-Dot <emmanuel.stephen68@gmail.com>
2025-05-29 18:48:06 -0500

    Properly added Angular frontend to main Praxii repo

---
+++ backend/src/main/java/com/praxii/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.praxii.controller;
+
+import com.praxii.model.User;
+import com.praxii.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
+
