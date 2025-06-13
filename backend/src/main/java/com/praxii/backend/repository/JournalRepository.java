1c31cf22e72bd3fbad210329b838c08c07bc0e56
Manii-Dot <emmanuel.stephen68@gmail.com>
2025-06-13 15:35:17 -0500

    last minute commit

--- backend/src/main/java/com/praxii/backend/repository/JournalRepository.java
+++ backend/src/main/java/com/praxii/backend/repository/JournalRepository.java
@@ -1,11 +1,10 @@
 package com.praxii.backend.repository;
 
 import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import com.praxii.backend.model.Journal;
 
 public interface JournalRepository extends MongoRepository<Journal, String> {
     List<Journal> findByUserIdOrderByCreatedAtDesc(String userId);
 }
+ 
