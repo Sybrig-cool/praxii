c9524c9cb7fb5d446e53e6526cb623f8a27ad07c
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2025-06-13 12:36:26 -0500

    test: add forms and http mocks

--- frontend/src/app/auth/signup/signup.component.spec.ts
+++ frontend/src/app/auth/signup/signup.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { SignupComponent } from './signup.component';
+import { AuthService } from '../auth.service';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  const authServiceMock = {
+    signup: jasmine.createSpy('signup').and.returnValue(of('success'))
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [ SignupComponent ],
+      imports: [ FormsModule, HttpClientTestingModule ],
+      providers: [ { provide: AuthService, useValue: authServiceMock } ]
     })
     .compileComponents();
 
